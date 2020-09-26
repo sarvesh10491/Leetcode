84ae764ce05f5bed35c3c0d81d5de15680d46d53
sarvesh10491 <sarvesh10491@gmail.com>
2020-09-26 10:25:49 -0700

    Added new Questions

--- Pattern_Based/6_Unique Paths.py
+++ Pattern_Based/6_Unique Paths.py
@@ -27,7 +27,6 @@ class Solution(object):
 
 
 
-
 # Combinatorial solution
 ''' Algorithm :
 Irrespective of which path we take, total distance we traverse is m-1 horizontally & (n-1) vertically => (m-1) + (n-1) = m+n-2
@@ -55,3 +54,42 @@ class Solution(object):
         :rtype: int
         """
         return (self.fact(m+n-2)) // (self.fact(m-1) * self.fact(n-1))
+
+
+
+
+# Dynamic programming solution
+''' Algorithm :
+We will set 1st row & column to 1 starting from top left cell of grid.
+Then for each remaining cell, we will calculate paths possible from that cell by adding available paths from cell row down & column down.
+The final value in right bottom cell of grid will be total paths which is expected answer.
+
+For m=3 & n=7 dp table in end will look like below.
+[
+[ 1 1 1 1 1 1 1]
+[ 1 2 3 4 5 6 7]
+[ 1 3 6 10 15 21 28]
+]
+The function returns value in the right down corner: 28
+
+'''
+#=========================
+
+class Solution:
+    def uniquePaths(self, m, n):
+        dp = [[None for _ in range(n)] for _ in range(m)]   # Empty dp table
+        k = min(m,n)    # Select min of rows or columns to iterate over
+
+        for d in range(k):
+            if d == 0:  # We will set 1st row & column to 1
+                for i in range(d,m):
+                    dp[i][d] = 1
+                for j in range(d,n):
+                    dp[d][j] = 1
+            else:
+                for i in range(d,m):
+                    dp[i][d] = dp[i - 1][d] + dp[i][d - 1]
+                for j in range(d,n):
+                    dp[d][j] = dp[d - 1][j] + dp[d][j - 1]
+
+        return dp[m - 1][n - 1]
