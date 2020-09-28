507948ecf95b421272d8c3bce4ccfd073d558b1f
sarvesh10491 <sarvesh10491@gmail.com>
2020-09-27 22:34:55 -0700

    Added new Questions

--- Arithmatic_Based/3_Add_Binary.py
+++ Arithmatic_Based/3_Add_Binary.py
@@ -9,6 +9,7 @@ class Solution(object):
         i,j = len(a)-1,len(b)-1
         summ,car = 0,0
         
+        # Loop to sum each position digit wise until both strings have digits
         while i>-1 and j>-1:
             summ = int(a[i])+int(b[j])+car
 
@@ -24,7 +25,8 @@ class Solution(object):
             i -= 1
             j -= 1
         
-        
+
+        # Loop to sum remaining digits in longer string until carry is 1
         while i>-1:
             summ = int(a[i])+car
             
@@ -33,7 +35,9 @@ class Solution(object):
                 car = 1
             elif summ < 2:
                 ans += str(summ)
+                ans += a[:i][::-1]
                 car = 0
+                break
             i -= 1
         
         
@@ -44,9 +48,12 @@ class Solution(object):
                 car = 1
             elif summ < 2:
                 ans += str(summ)
+                ans += b[:j][::-1]
                 car = 0
+                break
             j -= 1
         
+        # Add 1 if there is trailing carry in end
         if car:
             ans += "1"
         
